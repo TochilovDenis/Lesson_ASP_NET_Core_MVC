18d83b9a5c03bc9f80d7061958fc8cf171663ee0
TochilovDenis <denis-tochilov@yandex.ru>
2025-07-23 20:19:20 +0300

    https://metanit.com/sharp/aspnetmvc/2.1.php - Методы, которые не являются действиями - Атрибуты NonController, ActionName и NonAction - Типы запросов

--- MvcApp/Controllers/HomeController.cs
+++ MvcApp/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
- 
+
 namespace MvcApp.Controllers
 {
+
     public class HomeController : Controller
     {
         /*public IActionResult Index()
         {
             return View();
         }*/
-        public string Index()
-        {
-            return "Hello IT Academy-TOP 2";
-        }
+        [HttpGet]
+        public string Index() => "Hello IT Academy-TOP 2";
+
+        [HttpPost]
+        public string Hello() => "Hello IT Academy-TOP 5";
+
+        protected internal string HelloOne() => "Hello IT Academy-TOP 3";
+
+        [ActionName("Welcome")]
+        public string HelloTwo() => "Hello IT Academy-TOP 4";
+
+
+
+
     }
 }
