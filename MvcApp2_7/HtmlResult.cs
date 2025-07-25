f4ddde458eb72a4ceefc6641709cd600a1b68c3a
TochilovDenis <denis-tochilov@yandex.ru>
2025-07-25 20:41:35 +0300

    Проект предыдущего урока из McvApp2.6

---
+++ MvcApp2_7/HtmlResult.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcApp2_5
+{
+    public class HtmlResult : IActionResult
+    {
+        string htmlCode;
+        public HtmlResult(string html) => htmlCode = html;
+        public async Task ExecuteResultAsync(ActionContext context)
+        {
+            string fullHtmlCode = @$"<!DOCTYPE html>
+            <html>
+                <head>
+                    <title>Наш сайт!</title>
+                    <meta charset=utf-8 />
+                </head>
+                <body>{htmlCode}</body>
+            </html>";
+            await context.HttpContext.Response.WriteAsync(fullHtmlCode);
+        }
+    }
+}
