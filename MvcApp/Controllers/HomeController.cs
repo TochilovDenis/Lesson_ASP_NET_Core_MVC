19a3f0afbd78aa1e2497ea29e2fc094b79e16f65
TochilovDenis <denis-tochilov@yandex.ru>
2025-07-25 19:39:07 +0300

    Передача словарей Dictionary https://metanit.com/sharp/aspnetmvc/2.4.php

--- MvcApp/Controllers/HomeController.cs
+++ MvcApp/Controllers/HomeController.cs
@@ -191,7 +191,7 @@ namespace MvcApp.Controllers
 
 
     // Получение массивов
-    public class HomeController : Controller
+    /*public class HomeController : Controller
     {
         public async Task Index()
         {
@@ -214,6 +214,48 @@ namespace MvcApp.Controllers
             }
             return result;
         }
+    }*/
+
+
+    // Получение массивов
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public async Task Index()
+        {
+            string content = @"<form method='post'>
+                <p>
+                    Германия:
+                    <input type='text' name='items[germany]' />
+                </p>
+                <p>
+                    Франция:
+                    <input type='text' name='items[france]' />
+                </p>
+                <p>
+                    Испания:
+                    <input type='text' name='items[spain]' />
+                </p>
+                <p>
+                    <input type='submit' value='Отправить' />
+                </p>
+            </form>";
+            Response.ContentType = "text/html;charset=utf-8";
+            await Response.WriteAsync(content);
+        }
+        [HttpPost]
+        public string Index(Dictionary<string, string> items)
+        {
+            string result = "";
+            foreach (var item in items)
+            {
+                result = $"{result} {item.Key} - {item.Value}; ";
+            }
+            return result;
+        }
     }
+
+
+
 }
 
