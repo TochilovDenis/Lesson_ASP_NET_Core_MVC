bf623c06490dd74e6d628f1e493c55f674b817c1
TochilovDenis <denis-tochilov@yandex.ru>
2025-08-18 19:49:26 +0300

    Cпособы передачи данных из контроллера в представление:   * ViewData   * ViewBag
    https://metanit.com/sharp/aspnetmvc/3.3.php

---
+++ MvcApp3_3/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcApp.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // ViewData
+            // ViewData представляет словарь из пар ключ-значение:
+            ViewData["Message"] = "Hello AcademyTop";
+
+            // ViewBag
+            // ViewBag во многом подобен ViewData.Он позволяет определить различные свойства и присвоить им любое значение. 
+            ViewBag.Message = "Hello METANIT.COM";
+
+            ViewBag.People = new List<string> { "Tom", "Sam", "Bob" };
+
+            return View();
+        }
+    }
+}
+
+// Передача данных в представление
