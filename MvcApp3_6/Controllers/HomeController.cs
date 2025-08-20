c851e7a38e9ed4c6e76c6a143a7404a9eee7be94
TochilovDenis <denis-tochilov@yandex.ru>
2025-08-20 19:10:56 +0300

    Взят предыдущее 3_5

---
+++ MvcApp3_6/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+        public IActionResult About() => View();
+    }
+}
+
+// Передача данных в представление
