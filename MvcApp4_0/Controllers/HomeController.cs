d589a8ef92a9338830f3faf529f9c669777e4fa0
TochilovDenis <denis-tochilov@yandex.ru>
2025-08-20 20:57:06 +0300

    Необязательные параметры маршрутов https://metanit.com/sharp/aspnetmvc/4.2.php

--- MvcApp4_0/Controllers/HomeController.cs
+++ MvcApp4_0/Controllers/HomeController.cs
@@ -4,7 +4,15 @@ namespace MvcApp4_0.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index(int id) => $"Index Page. Id: {id}";
+        //public string Index(int id) => $"Index Page. Id: {id}";
+
+        public string Index(int? id)
+        {
+            if (id is not null) return $"Product Id: {id}";
+
+            return "Product List";
+        }
+
         public string About(string name, int age) => $"About Page. Name: {name}  Age: {age}";
     }
 }
--- MvcApp4_0/Program.cs
+++ MvcApp4_0/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // устанавливаем сопоставление маршрутов с контроллерами
-app.MapControllerRoute(name: "default", pattern: "{controller}/{action}/{id}");
+app.MapControllerRoute(name: "default", pattern: "{controller}/{action}/{id?}");
 app.MapControllerRoute(name: "name_age", pattern: "{controller}/{action}/{name}/{age}");
 
 app.Run();
