83a9015aaedf9d22e7526a3b344dc607c4840ae8
TochilovDenis <denis-tochilov@yandex.ru>
2025-08-20 19:39:09 +0300

    Взят предыдущее 3_6

---
+++ MvcApp3_7/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+        public IActionResult About() => View();
+
+        public IActionResult Hello()
+        {
+            return PartialView();
+        }
+    }
+}
+
+// Передача данных в представление
---
+++ MvcApp3_7/Models/Person.cs
@@ -0,0 +1,4 @@
+﻿namespace MvcApp3_7.Models
+{
+    public record class Person(string Name, int Age);
+}
