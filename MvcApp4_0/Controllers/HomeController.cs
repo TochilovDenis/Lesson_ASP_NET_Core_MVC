05cd8eef085af714514b7bd96b0a599c366769e2
TochilovDenis <denis-tochilov@yandex.ru>
2025-08-20 20:47:22 +0300

    Определение маршрутов. https://metanit.com/sharp/aspnetmvc/4.2.php

---
+++ MvcApp4_0/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcApp4_0.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index() => "Index Page";
+        public string About() => "About Page";
+    }
+}
