66b727e4fa5e256cba2c12235a09f244ff372e9f
TochilovDenis <denis-tochilov@yandex.ru>
2025-09-03 18:55:26 +0300

    HTML-хелперы https://metanit.com/sharp/aspnetmvc/6.1.php

---
+++ MvcApp6_0/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MvcApp5_0.Models;
+
+namespace MvcApp5_0.Controllers
+{
+    [Route("{controller=Home}/{action=index}")]
+    public class HomeController : Controller
+    {
+
+        IEnumerable<Person> people = new List<Person>
+        {
+            new Person(1, "Tom", 37),
+            new Person(2, "Bob", 41),
+            new Person(3, "Sam", 28)
+        };
+
+        [HttpGet]
+        //[Route("Home/Index")]
+        public IActionResult Index()
+        {
+            ViewBag.HeaderTitle = "Добро пожаловать!";
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Index(string[] languages)
+        //{
+        //    string result = "Вы выбрали:";
+        //    foreach (string lang in languages)
+        //    {
+        //        result = $"{result} {lang};";
+        //    }
+        //    return Content(result);
+        //}
+
+        [HttpPost]
+        public IActionResult Index(string name, string password, int age, string comment)
+        {
+            // Обработка пользовательской формы
+            return Content($"User Name: {name}   Password: {password}   Age: {age}  Comment: {comment}");
+
+        }
+
+        [HttpGet]
+        //[Route("Home/About")]
+        public IActionResult About()
+        {
+            ViewBag.Title = "About";
+            ViewBag.HeaderTitle = "Person";
+            return View("About", people);
+        }
+
+
+        [HttpGet]
+        //[Route("Home/Contact/{age:int?}/{name?}")]
+        public IActionResult Contact(int age = 5, string name = "Dex")
+        {
+            return Content($"Home/Contact action - Name: {name}, Age: {age}");
+        }
+    }
+}
---
+++ MvcApp6_0/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace MvcApp5_0.Models
+{
+    //public record class Person(int Id, string Name, int Age);
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int Age { get; set; }
+        public Person(int id, string name, int age)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
