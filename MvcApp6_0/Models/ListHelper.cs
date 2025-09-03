854600e1e6a9560fe52112e1826e65a7c9d3eb3a
TochilovDenis <denis-tochilov@yandex.ru>
2025-09-03 19:39:13 +0300

    Реализация HTML-хелпера с использованием TagBuilder
    - Добавлен класс ListHelper со статическим методом CreateList
    - Использован Microsoft.AspNetCore.Mvc.Rendering.TagBuilder для генерации HTML
    - Реализовано создание списков (ul/li) из массива строк
    - Добавлена поддержка CSS классов через Attributes
    - Обеспечено корректное преобразование в HtmlString
    
    https://metanit.com/sharp/aspnetmvc/6.1.php

--- MvcApp6_0/Models/ListHelper.cs
+++ MvcApp6_0/Models/ListHelper.cs
@@ -1,20 +1,27 @@
-﻿using Microsoft.AspNetCore.Html;            // для HtmlString
+﻿using Microsoft.AspNetCore.Html;        // для HtmlString
 using Microsoft.AspNetCore.Mvc.Rendering;   // для IHtmlHelper
-
-namespace MvcApp6_0.Models
+using System.Text.Encodings.Web;    // для HtmlEncoder
+ 
+namespace MvcApp6_0
 {
-    //[Route("/Home/ListCountries")]
     public static class ListHelper
-    {        
+    {
         public static HtmlString CreateList(this IHtmlHelper html, string[] items)
         {
-            string result = "<ul>";
+            TagBuilder ul = new TagBuilder("ul");
+
             foreach (string item in items)
             {
-                result = $"{result}<li>{item}</li>";
+                TagBuilder li = new TagBuilder("li");
+                // добавляем текст в li
+                li.InnerHtml.Append(item);
+                // добавляем li в ul
+                ul.InnerHtml.AppendHtml(li);
             }
-            result = $"{result}</ul>";
-            return new HtmlString(result);
+            ul.Attributes.Add("class", "itemsList");
+            using var writer = new StringWriter();
+            ul.WriteTo(writer, HtmlEncoder.Default);
+            return new HtmlString(writer.ToString());
         }
     }
 }
