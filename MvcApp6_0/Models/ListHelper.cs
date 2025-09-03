4ebfc23add7176bfa835330bbd1d542fa598fd95
TochilovDenis <denis-tochilov@yandex.ru>
2025-09-03 20:57:51 +0300

    feat(HTMLHelpers): реализован метод CreateList для интерактивных викторин
    - Добавлен метод-расширение для IHtmlHelper для генерации списков вопросов
    - Каждый элемент списка содержит: текст вопроса, поле ввода для ответа и чекбокс
    - Поддержка пользовательских заголовков разделов через параметр 'topic'
    - Использование TagBuilder для безопасной генерации HTML с правильным кодированием
    - Включены примеры использования в Razor-представлении с тестовыми данными
    - Добавлен CSS-класс 'itemsList' для будущих вариантов стилизации
    - Обеспечена защита от XSS через HtmlEncoder.Default

--- MvcApp6_0/Models/ListHelper.cs
+++ MvcApp6_0/Models/ListHelper.cs
@@ -6,7 +6,7 @@ namespace MvcApp6_0
 {
     public static class ListHelper
     {
-        public static HtmlString CreateList(this IHtmlHelper html, string[] items)
+        /*public static HtmlString CreateList(this IHtmlHelper html, string[] items)
         {
             TagBuilder ul = new TagBuilder("ul");
 
@@ -17,11 +17,72 @@ namespace MvcApp6_0
                 li.InnerHtml.Append(item);
                 // добавляем li в ul
                 ul.InnerHtml.AppendHtml(li);
+            }*/
+
+       
+        public static HtmlString CreateList(this IHtmlHelper html, string[] items, string topic)
+        {
+            // Создаем основной div контейнер
+            var containerDiv = new TagBuilder("div");
+
+            // Создаем заголовок h2
+            var h2 = new TagBuilder("h2");
+            h2.InnerHtml.Append(topic);
+
+            // Добавляем заголовок в контейнер
+            containerDiv.InnerHtml.AppendHtml(h2);
+
+            foreach (string question in items)
+            {
+                // Создаем ul для каждого вопроса
+                var ul = new TagBuilder("ul");
+                ul.Attributes.Add("class", "itemsList");
+
+                // Первый li с вопросом
+                var liQuestion = new TagBuilder("li");
+                liQuestion.InnerHtml.Append(question);
+                ul.InnerHtml.AppendHtml(liQuestion);
+
+                // Второй li с полем ввода "Ответ"
+                var liAnswer = new TagBuilder("li");
+                var inputAnswer = new TagBuilder("input");
+                inputAnswer.Attributes.Add("value", "Ответ");
+                inputAnswer.Attributes.Add("type", "text");
+                liAnswer.InnerHtml.AppendHtml(inputAnswer);
+                ul.InnerHtml.AppendHtml(liAnswer);
+
+                // Третий li с чекбоксом
+                var liCheckbox = new TagBuilder("li");
+                var inputCheckbox = new TagBuilder("input");
+                inputCheckbox.Attributes.Add("type", "checkbox");
+                liCheckbox.InnerHtml.AppendHtml(inputCheckbox);
+                ul.InnerHtml.AppendHtml(liCheckbox);
+
+                // Добавляем ul в контейнер
+                containerDiv.InnerHtml.AppendHtml(ul);
             }
-            ul.Attributes.Add("class", "itemsList");
+
             using var writer = new StringWriter();
-            ul.WriteTo(writer, HtmlEncoder.Default);
+            containerDiv.WriteTo(writer, HtmlEncoder.Default);
             return new HtmlString(writer.ToString());
         }
+
+
+        /*public static HtmlString CreateList(this IHtmlHelper html, string[] items, string topic)
+       {
+           string result = "<div><h2>" + topic + "</h2></div>";
+
+           foreach (string question in items)
+           {
+               result = "${result}<ul class=\"itemsList\">" +
+                     $"<li>{question}</li>" +
+                     $"<li><input value=\"Ответ\"></li>" +
+                     $"<li>< input type =\"checkbox\"></li>" +
+                     $"</ul>";
+           }
+
+           result = $"{result}</div>";
+           return new HtmlString(result);
+       }*/
     }
 }
